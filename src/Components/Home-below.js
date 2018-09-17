04cd58865d4e97377a1b06218f9a939ead391f41
CSamboni <elementrixz512@gmail.com>
2018-09-16 19:15:19 -0500

    Modificando a repo de pruebas

---
+++ src/Components/Home-below.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class HomeBelow extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default HomeBelow;
