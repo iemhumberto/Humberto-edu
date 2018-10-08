1c066479887faf75ae28e954102cc7a354600b9c
CSamboni <elementrixz512@gmail.com>
2018-10-08 16:57:00 -0500

    Seccion de escritura- estudiantes

--- src/Components/Estudiantes/ToolsEstudiar.js
+++ src/Components/Estudiantes/ToolsEstudiar.js
@@ -18,6 +18,11 @@ const swipe = 'https://i.pinimg.com/originals/a6/7a/d9/a67ad9821f009561f625c6697
 const Trello = 'https://png.icons8.com/color/1600/trello.png';
 const Mtodo = 'https://mk0intowindows84fvao.kinstacdn.com/wp-content/uploads/2017/04/delete-Mic' +
         'rosfot-To-Do-account_thumb.png';
+/*Herramientas para escritura y notas */
+
+const Evernote = 'https://s24953.pcdn.co/blog/wp-content/uploads/2018/01/evernote-logo-wide.png';
+const Medium = 'https://media.wired.com/photos/5a55a95af41e4c2cd9ee6cd8/master/pass/Medium-logo-' +
+        'canvas.jpg';
 
 class ToolsEstudiar extends Component {
     render() {
@@ -76,7 +81,7 @@ class ToolsEstudiar extends Component {
                             <div className="button">Usar herramienta</div>
                         </a>
                     </div>
-                    {/*Canvas herramientas*/}
+                    {/*Canva herramientas*/}
                     <div className="tools-presentaciones-canva">
                         <img className="GSlides" src={Canva} alt="Google slides presentaciones"/>
                         <p>Diseño de presentaciones. Interesantes y atractivas.</p>
@@ -148,6 +153,48 @@ class ToolsEstudiar extends Component {
                     </div>
                     <div className="title titulo-productividad">
                         <p className="productividad-fijo">Productividad</p>
+                    </div>
+                </section>
+                {/*Herramientas par escritura*/}
+                {/*Evernote*/}
+                <section className="row escritura-tools">
+                    <div className="col-xs-2 tools-escritura-evernote">
+                        <img className="Evernote" src={Evernote} alt="Evernote. Escritura"/>
+                        <p>Evernote te ayuda a capturar y establecer la prioridad de las ideas,
+                            proyectos y listas de tareas, para que nada se te pase por alto.</p>
+                        <a
+                            style={{
+                            'display': 'flex',
+                            'marginTop': '25%',
+                            'justifyContent': 'center'
+                        }}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://evernote.com/intl/es/">
+                            <div className="button">Usar herramienta</div>
+                        </a>
+                    </div>
+                    {/*Medium post*/}
+                    <div className="tools-escritura-medium">
+                        <img
+                            className="Medium"
+                            src={Medium}
+                            alt="Medium. Publica tu conocimiento."/>
+                        <p>Agende y agregue en lista sus labores. Sea organizado.</p>
+                        <a
+                            style={{
+                            'display': 'flex',
+                            'marginTop': '25%',
+                            'justifyContent': 'center'
+                        }}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://medium.com/">
+                            <div className="button">Usar herramienta</div>
+                        </a>
+                    </div>
+                    <div className="title titulo-escritura">
+                        <p className="escritura-fijo">Escritura</p>
                     </div>
                 </section>
             </section>
