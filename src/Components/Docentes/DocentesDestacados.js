c6601a0a10e46b438e95fc46429d59e7a2514190
CSamboni <elementrixz512@gmail.com>
2018-10-27 17:31:15 -0500

    Agregando sesion de docentes/innovadores/destacados

---
+++ src/Components/Docentes/DocentesDestacados.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class DocentesDestacados extends Component {
+    state = {
+        Perfiles = []
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default DocentesDestacados;
---
+++ src/Components/Docentes/DocentesInnovadores.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class DocentesInnovadores extends Component {
+    state = {
+        Perfiles = []
+    }
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default DocentesInnovadores;
